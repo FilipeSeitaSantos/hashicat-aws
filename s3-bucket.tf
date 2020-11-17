bc185e5148b31be33f8d979f8013a0ca5098c38b
Filipe <filipeseitasantos@gmail.com>
2020-11-17 20:21:24 +0000

    Added the first tag

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/CloudBase/s3-bucket/aws"
   version = "1.16.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
+  #bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = "FSS_S3"
   versioning = {
