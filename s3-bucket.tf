6431ecf30bbd192662470186fea76b4c687a25a8
Filipe <filipeseitasantos@gmail.com>
2020-11-17 20:30:44 +0000

    Added the s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
 
 module "s3-bucket" {
   source  = "app.terraform.io/CloudBase/s3-bucket/aws"
-  version = "1.16.0"
+  version = "1.15.0"
   # insert required variables here
-  bucket = "FSS-teste-my-s3-bucket"
-  acl    = "private"
-  #bucket_prefix = "FSS_S3"
-  versioning = {
-    enabled = true
-  }
 }
