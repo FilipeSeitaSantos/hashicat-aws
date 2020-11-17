c1132c659c7525c82ff87500761b9d0ab7783609
root <root@p-wqemlzwhfmgu-workstation.c.instruqt-prod.internal>
2020-11-17 18:12:50 +0000

    .

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "CloudBase"
+    organization = "YOURORGANIZATION"
     workspaces {
-      name = "hashicat-aws"
+      name = "YOURWORKSPACE"
     }
   }
 }
