5e8e53f0908891dd8c11e8c71874103cf09fc56a
FilipeSeitaSantos <73106894+FilipeSeitaSantos@users.noreply.github.com>
2020-11-18 14:50:22 +0000

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -5,7 +5,7 @@ module "s3-bucket" {
   # insert required variables here
 }
 
-  module "s3-bucket" {
+  module "s3-bucket2" {
   source  = "app.terraform.io/CloudBase/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
